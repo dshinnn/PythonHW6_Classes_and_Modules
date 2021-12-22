3895e5ba6cae544d070c896bdcbfc6e52e86990c
David Shin <shin.david93@gmail.com>
2021-12-22 08:17:11 -0800

    moved phone number attribute from Employee class to Sales class

--- geometry.py
+++ geometry.py
@@ -3,5 +3,5 @@ from math import pi, sqrt
 def area_of_circle(radius):
     return pi * radius ** 2
 
-def find_hypotenus(a, b):
-    return sqrt(a+b)
+def find_hypotenuse(a, b):
+    return sqrt(a**2 + b**2)
